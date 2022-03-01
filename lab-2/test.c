78188be68711dc7c5c2900586106c770cbb5c598
Mark <mlgameng@gmail.com>
2022-03-01 17:51:12 -0600

    fixed test

--- lab-2/test.c
+++ lab-2/test.c
@@ -1,20 +1,20 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-#include <stdlib.h>
 
 int main(int argc, char *argv[1]){
     //printf("hello world\n");
     
     // list of childs, so i can reference their pids
     int pids[3] = {0, 0, 0};
+    int exitStatuses[3] = {5, 22, 13};
+    
     for(int i = 0; i < 3; i++){
         pid = fork();
         pids[i] = pid;
         if (pid == 0){
-            int rng = rand() % 20 + 1; // random from 1 to 20
-            printf("(Child, PID = %d) exiting with status %d", pid, rng)
-            exitStatus(rng);
+            printf("(Child, PID = %d) exiting with status %d", pid, exitStatuses[i])
+            exitStatus(exitStatuses[i]);
         }
     }
     
