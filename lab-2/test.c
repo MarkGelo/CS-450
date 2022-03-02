fa9b37aeaea134f626386916b9747990b7ca5a16
Mark <mlgameng@gmail.com>
2022-03-01 18:34:15 -0600

    test

--- lab-2/test.c
+++ lab-2/test.c
@@ -36,6 +36,35 @@ int main(int argc, char *argv[1]){
         pid = waitStatus(&exitStatus_);
         printf(1, "(Parent) Child with PID = %d exited with status %d \n", pid, exitStatus_);
     }
+
+    printf(1, "\n")
+    printf(1, "Testing waitpid\n");
+    // list of childs, so i can reference their pids
+    int pids[3] = {0, 0, 0};
+    int exitStatuses[3] = {5, 22, 13};
+    int options[3] = {7, 6, 5}
+    int pid;
+    int exitStatus_;
+
+    for(int i = 0; i < 3; i++){
+        pid = fork();
+        if (pid < 0){ // negative pid means fork failed
+            printf(1, "fork failed. Retry test\n");
+            exit();
+        }
+        if (pid == 0){ // child has their pid = 0
+            printf(1, "(Child) exiting with status %d \n", exitStatuses[i]);
+            exitStatus(exitStatuses[i]);
+        }
+        pids[i] = pid;
+    }
+
+    sleep(2); // give time so most childs finish
+    for(int i = 0; i < 3; i++){
+        pid = waitpid(pids[i], &exitStatus_, options[i]);
+        printf(1, "(Parent) Child with PID = %d exited with status %d \n", pid, exitStatus_);
+    }
+
     
     exit();
 }
