641670b76555979fe3bf1cae939e82bd9650a7c6
Mark <mlgameng@gmail.com>
2022-03-31 16:30:25 -0500

    lab3 added

---
+++ lab-3/sysproc.c
@@ -0,0 +1,136 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+// lab 2
+int sys_exitStatus(void)
+{
+  int status;
+
+  if (argint(0, &status) < 0){ // pass in an integer, passing arguments into a kernel function - https://stackoverflow.com/questions/27068394/how-to-pass-a-value-into-a-system-call-function-in-xv6
+    return -1;
+  }
+
+  exitStatus(status);
+
+  return 0; // not reached
+}
+
+//sysfile.c has code that uses argptr
+
+int sys_waitStatus(void){
+  int *status;
+  
+  if (argptr(0, (void *) &status, sizeof(*status)) < 0){
+    return -1;
+  }
+
+  return waitStatus(status);
+}
+
+int sys_waitpid(void){
+  int pid;
+  int options;
+  int *status;
+
+  // pid is 0, status 1, options 2. cuz waitpid(pid, status, options)
+  if (argint(0, &pid) < 0){
+    return -1;
+  }
+  if (argptr(1, (void *) &status, sizeof(*status)) < 0){
+    return -1;
+  }
+  if (argint(2, &options) < 0){
+    return -1;
+  }
+
+  return waitpid(pid, status, options);
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
---
+++ lab-3/test.c
@@ -0,0 +1,66 @@
+#include "param.h"
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
+
+int main(int argc, char *argv[1]){
+    //printf(1, "hello world\n");
+    
+    printf(1, "Testing exitStatus(int status) and waitStatus(int *status)\n");
+    // list of childs, so i can reference their pids
+    //int pids[3] = {0, 0, 0};
+    int exitStatuses[3] = {5, 22, 13};
+    int pid;
+    int exitStatus_;
+
+    for(int i = 0; i < 3; i++){
+        pid = fork();
+        if (pid < 0){ // negative pid means fork failed
+            printf(1, "fork failed. Retry test\n");
+            exit();
+        }
+        //pids[i] = pid;
+        if (pid == 0){ // child has their pid = 0
+            printf(1, "(Child) exiting with status %d \n", exitStatuses[i]);
+            exitStatus(exitStatuses[i]);
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        pid = waitStatus(&exitStatus_);
+        printf(1, "(Parent) Child with PID = %d exited with status %d \n", pid, exitStatus_);
+    }
+
+    printf(1, "\n");
+    printf(1, "Testing waitpid\n");
+    // list of childs, so i can reference their pids
+    int pids[3] = {0, 0, 0};
+    int options[3] = {7, 6, 5};
+
+    for(int i = 0; i < 3; i++){
+        pid = fork();
+        if (pid < 0){ // negative pid means fork failed
+            printf(1, "fork failed. Retry test\n");
+            exit();
+        }
+        if (pid == 0){ // child has their pid = 0
+            printf(1, "(Child) exiting with status %d \n", exitStatuses[i]);
+            exitStatus(exitStatuses[i]);
+        }
+        pids[i] = pid;
+    }
+
+    for(int i = 0; i < 3; i++){
+        printf(1, "(Parent) Waiting for child with PID = %d to exit \n", pids[2 - i]);
+        pid = waitpid(pids[2 - i], &exitStatus_, options[i]);
+        printf(1, "(Parent) Child with PID = %d exited with status %d \n", pid, exitStatus_);
+    }
+
+    
+    exit();
+}
