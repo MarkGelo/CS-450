dcd2bd8708b02e00ef83902d5e3228caedee6a6a
Mark <mlgameng@gmail.com>
2022-04-01 20:05:54 -0500

    test

--- lab-3/test.c
+++ lab-3/test.c
@@ -5,62 +5,10 @@
 #include "fs.h"
 #include "fcntl.h"
 #include "syscall.h"
-#include "traps.h"
+//#include "traps.h"
 #include "memlayout.h"
 
 int main(int argc, char *argv[1]){
-    //printf(1, "hello world\n");
-    
-    printf(1, "Testing exitStatus(int status) and waitStatus(int *status)\n");
-    // list of childs, so i can reference their pids
-    //int pids[3] = {0, 0, 0};
-    int exitStatuses[3] = {5, 22, 13};
-    int pid;
-    int exitStatus_;
-
-    for(int i = 0; i < 3; i++){
-        pid = fork();
-        if (pid < 0){ // negative pid means fork failed
-            printf(1, "fork failed. Retry test\n");
-            exit();
-        }
-        //pids[i] = pid;
-        if (pid == 0){ // child has their pid = 0
-            printf(1, "(Child) exiting with status %d \n", exitStatuses[i]);
-            exitStatus(exitStatuses[i]);
-        }
-    }
-
-    for(int i = 0; i < 3; i++){
-        pid = waitStatus(&exitStatus_);
-        printf(1, "(Parent) Child with PID = %d exited with status %d \n", pid, exitStatus_);
-    }
-
-    printf(1, "\n");
-    printf(1, "Testing waitpid\n");
-    // list of childs, so i can reference their pids
-    int pids[3] = {0, 0, 0};
-    int options[3] = {7, 6, 5};
-
-    for(int i = 0; i < 3; i++){
-        pid = fork();
-        if (pid < 0){ // negative pid means fork failed
-            printf(1, "fork failed. Retry test\n");
-            exit();
-        }
-        if (pid == 0){ // child has their pid = 0
-            printf(1, "(Child) exiting with status %d \n", exitStatuses[i]);
-            exitStatus(exitStatuses[i]);
-        }
-        pids[i] = pid;
-    }
-
-    for(int i = 0; i < 3; i++){
-        printf(1, "(Parent) Waiting for child with PID = %d to exit \n", pids[2 - i]);
-        pid = waitpid(pids[2 - i], &exitStatus_, options[i]);
-        printf(1, "(Parent) Child with PID = %d exited with status %d \n", pid, exitStatus_);
-    }
-
-    
+    printf(1, "hello world\n");
     exit();
 }
